d62d5aa512a0709525608e320581f5f3107739ef
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2025-10-29 09:56:44 +0900

    feat : 로또 구입비 입력 및 로또 개수 계산 기능
    로또 구입비를 문자열로 입력받아 Integer 형으로 변경하는 기능을 추가하였습니다. 입력된 돈을 기반으로 로또의 개수를 계산하는 기능도 추가하였습니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,21 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    public void run(){
+        System.out.println("구입금액을 입력해 주세요.");
+        Integer money = Integer.parseInt(Console.readLine());
+        // 구입한 로또의 개수
+        Integer numOfLotto = money / 1000; // 로또의 개수만큼 변경
+    }
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        new Application().run();
     }
 }
