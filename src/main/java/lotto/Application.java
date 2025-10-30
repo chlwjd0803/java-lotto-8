2ca4f065cffb662e58ec66e7e3c6cafb90084e3c
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2025-10-30 10:40:00 +0900

    feat : 로또번호 입력 관련 예외 추가 및 리펙토링
    로또 번호에 대해 다음과 같은 예외들을 구현하여 메소드를 분리하였습니다.
    
    1. 쉼표구분자를 이용해서 숫자를 나누었는지 검사
    2. List에 원소를 삽입하며 정수형태가 맞는지 검사
    3. 여섯개의 번호만을 입력했는지 검사
    4. 1~45 범위내에 번호들로만 구성되어 있는지 검사
    5. 번호들의 중복되지 않는지 검사

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,10 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Application {
     private static final String NUMBER_LIST_REGEX = "^\\d+(,\\d+)*$";
@@ -27,42 +24,87 @@ public class Application {
         }
     }
 
+    // 정규식을 이용하여 쉼표 구분자로 숫자를 정리하였는지
+    private Boolean checkLottoInputFormat(String input){
+        try{
+            if(!input.matches(NUMBER_LIST_REGEX))
+                throw new IllegalArgumentException("입력 형태가 올바르지 않음");
+            return true;
+        } catch (IllegalArgumentException e){
+            System.out.println("[ERROR] 입력 형태가 올바르지 않습니다. 쉼표 구분자를 구분하여 정수를 입력해주세요.");
+            return false;
+        }
+    }
+
+    // 입력한 로또 번호가 정수형태인지 검사
+    private Boolean insertWinningNumbers(String input, List<Integer> winningNumbers){
+        String[] inputSplit = input.split(",");
+
+        try{
+            for(String number : inputSplit){
+                winningNumbers.add(Integer.parseInt(number));
+            }
+            return true;
+        } catch (NumberFormatException e){
+            System.out.println("[ERROR] 입력 로또번호가 올바른 정수형태가 아닙니다.");
+            return false;
+        }
+    }
+
+    // 로또 번호가 6개로 입력되었는지 검사
+    private Boolean checkWinningNumbersIsSix(List<Integer> winningNumbers){
+        try{
+            if(winningNumbers.size() != 6)
+                throw new IllegalArgumentException("로또번호 6개 불일치");
+            return true;
+        } catch (IllegalArgumentException e){
+            System.out.println("[ERROR] 로또 번호는 6개여야 합니다.");
+            return false;
+        }
+    }
+
+    private Boolean checkWinningNumbersIsUnique(List<Integer> winningNumbers){
+        try{
+            HashSet<Integer> winningNumberSet = new HashSet<>(winningNumbers);
+            if(winningNumberSet.size() != 6)
+                throw new IllegalArgumentException("중복된 원소가 존재함");
+            return true;
+        } catch (IllegalArgumentException e){
+            System.out.println("[ERROR] 중복된 번호를 입력할 수 없습니다.");
+            return false;
+        }
+    }
+
+    private Boolean checkWinningNumbersInRange(List<Integer> winningNumbers){
+        try{
+            for(Integer number : winningNumbers){
+                if(number < 1 || number > 45)
+                    throw new IllegalArgumentException("범위를 넘어가는 원소가 존재");
+            }
+            return true;
+        } catch (IllegalArgumentException e){
+            System.out.println("[ERROR] 번호의 범위인 1~45를 넘어갈 수 없습니다.");
+            return false;
+        }
+    }
+
     // 당첨 번호 입력
     private List<Integer> retWinningLotto(){
         while(true){
             System.out.println("당첨 번호를 입력해 주세요.");
             String input = Console.readLine();
-
-            try{
-                if(!input.matches(NUMBER_LIST_REGEX))
-                    throw new IllegalArgumentException("입력 형태가 올바르지 않음");
-            } catch (IllegalArgumentException e){
-                System.out.println("[ERROR] 입력 형태가 올바르지 않습니다. 쉼표 구분자를 구분하여 정수를 입력해주세요.");
-                continue;
-            }
-
-            String[] inputSplit = input.split(",");
             List<Integer> winningNumbers = new ArrayList<>();
 
-            try{
-                for(String number : inputSplit){
-                    winningNumbers.add(Integer.parseInt(number));
-                }
-            } catch (NumberFormatException e){
-                System.out.println("[ERROR] 입력 로또번호가 올바른 정수형태가 아닙니다.");
-                continue;
-            }
+            if(!checkLottoInputFormat(input)) continue; // 정규식을 따르는지 검사
+            if(!insertWinningNumbers(input, winningNumbers)) continue; // List에 원소를 집어넣으며 정수형태 검증
+            if(!checkWinningNumbersIsSix(winningNumbers)) continue; // 여섯개의 번호만 입력했는지 검사
+            if(!checkWinningNumbersInRange(winningNumbers)) continue; // 1~45 범위의 번호들만 입력했는지 검사
+            if(!checkWinningNumbersIsUnique(winningNumbers)) continue; // 번호들이 중복됨이 있는지 검사
 
-            try{
-                if(winningNumbers.size() != 6)
-                    throw new IllegalArgumentException("로또번호 6개 불일치");
-                return winningNumbers;
-            } catch (IllegalArgumentException e){
-                System.out.println("[ERROR] 로또 번호의 개수가 6개가 아닙니다.");
-            }
-
+            return winningNumbers;
         }
     }
+
 
     // 보너스 번호 입력
     private Integer retBonusNumber(){
